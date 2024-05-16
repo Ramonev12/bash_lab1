0bda706afd1a34b5f641f5e6dc9c65849ca8bde9
Ramone Vaultz <ramonevaultz6@gmail.com>
2024-05-15 21:55:16 -0400

    initial commit

---
+++ new_file.py
@@ -0,0 +1,14 @@
+# printing to CLI
+print("Hissss..")
+
+# Print multiple lines
+print("The dog ran across the street. \nHe was chasing a car.")
+
+#Using the 'end' keyword to change the ending of a line to 
+#print("My name", "Python%")
+#print("Monty Python.")
+
+#print("My", "name", "is", "Monty", "Python", sep="-")
+
+# combine ' sep ' and end keyword in print statement.
+print("My", "name", "is", sep="_", end="&")
---
+++ warmup.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+ # Create a variable with a number and print the variable to the screen
+# user_name = 'ramone12'
+# print(user_name)
+
+# Get the user to input a float number and covert to an interger
+# x = input('enter a number')
+# print(float(x))
+# Create if else statement to see if user likes dogs
+# dog = input('Do you like dogs\n')
+# if dog == 'yes':
+    print("Yes i like dogs")
+else:
+    print("No i dont like dogs")
+# Create while loop to go as long as user input is not 5
+user_input = int(input('Please enter a number'))
+while user_input != 5:
+    user_input = int(input('Please enter a number'))
+else:
+    print("Congratulations")
+for i in range(1,11):
+    print('Hola')
